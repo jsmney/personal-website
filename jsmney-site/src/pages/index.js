64dd5559ce3f8b1e9584d1223e39637f4c90a3c7
Jasmine Wang <jasminey.tea@gmail.com>
2020-01-16 12:05:53 -0600

    nearly there

--- jsmney-site/src/pages/index.js
+++ jsmney-site/src/pages/index.js
@@ -5,6 +5,13 @@ import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
+import Coreo from "../images/coreo.png"
+import Facestuff from "../images/facestuff.jpg"
+import Coconuts from "../images/coconutsinc.png"
+import Nacwala from "../images/nacwala.png"
+import EastWest from "../images/eastwest.jpg"
+import TinyGH from "../images/gh-tiny.png"
+
 const IndexPage = () => (
   <Layout>
     <SEO title="Jasmine Wang" />
@@ -24,7 +31,109 @@ const IndexPage = () => (
     </div>
     <hr />
     <h2>Projects</h2>
-    <h3>Coreo</h3>
+    <div className="projects">
+      <div>
+        <a href="https://coreo-ai.herokuapp.com/">
+          <img src={Coreo} alt="Coreo" className="project-image" />
+        </a>
+        <div>
+          <h3>
+            <a href="https://coreo-ai.herokuapp.com/">Coreo</a>
+          </h3>
+          <h4>choreography assistance web app using machine learning</h4>
+          <p>
+            Coreo provides a source of automated feedback for dancers on their
+            choreography between rehearsals.
+          </p>
+          <p>
+            Coreo uses TensorFlow's PoseNet pose estimation library, web
+            workers, and video.js / videojs-record.
+          </p>
+          <p>
+            <a href="https://github.com/brain-dance/brain-dancer">
+              <img src={TinyGH} alt="Github Repo" className="icon" />
+            </a>
+          </p>
+        </div>
+      </div>
+      <div>
+        <a href="https://www.youtube.com/watch?v=ugCaQ_JWw2s">
+          <img src={Facestuff} alt="Facestuff" className="project-image" />
+        </a>
+        <div>
+          <h3>
+            <a href="https://www.youtube.com/watch?v=ugCaQ_JWw2s">Facestuff</a>
+          </h3>
+          <h4>toy camera mobile app using face detection</h4>
+          <p>
+            Facestuff was built in a 3 day hackathon using React Native, Expo,
+            and MongoDB.
+          </p>
+          <p>
+            <a href="https://github.com/brain-dance/brain-dancer">
+              <img src={TinyGH} alt="Github Repo" className="icon" />
+            </a>
+          </p>
+        </div>
+      </div>
+      <div>
+        <a href="http://coconutsforsale.herokuapp.com/">
+          <img src={Coconuts} alt="Facestuff" className="project-image" />
+        </a>
+        <div>
+          <h3>
+            <a href="http://coconutsforsale.herokuapp.com/">Coconuts, Inc</a>
+          </h3>
+          <h4>full stack e-commerce web application</h4>
+          <p>
+            Coconuts, Inc is made in React, Redux, Express, Sequelize, and
+            PostreSQL. Checkout using Stripe API.
+          </p>
+          <p>
+            <a href="https://github.com/team-coconut-tiki/grace-shopper">
+              <img src={TinyGH} alt="Github Repo" className="icon" />
+            </a>
+          </p>
+        </div>
+      </div>
+      <div>
+        <a href="https://www.nacwala.com/">
+          <img src={Nacwala} alt="NACWALA" className="project-image" />
+        </a>
+        <div>
+          <h3>
+            <a href="https://www.nacwala.com/">NACWALA</a>
+          </h3>
+          <h4>
+            official website for the North America Chinese Writers Association
+            Los Angeles
+          </h4>
+          <p>Front end development and design using Bootstrap, HTML5, CSS.</p>
+          <p>
+            <a href="https://github.com/jsmney/nacwala-2">
+              <img src={TinyGH} alt="Github Repo" className="icon" />
+            </a>
+          </p>
+        </div>
+      </div>
+      <div>
+        <a href="http://www.eastwestcultureandart.com/">
+          <img src={EastWest} alt="East West" className="project-image" />
+        </a>
+        <div>
+          <h3>
+            <a href="http://www.eastwestcultureandart.com/">
+              East West Culture &amp; Art Center
+            </a>
+          </h3>
+          <h4>official website for organization based in San Diego</h4>
+          <p>
+            Front end development and design using Bootstrap, jQuery, HTML5,
+            CSS.
+          </p>
+        </div>
+      </div>
+    </div>
     {/* <Link to="/page-2/">Go to page 2</Link> */}
   </Layout>
 )
